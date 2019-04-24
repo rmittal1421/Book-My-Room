025b264dbce93872bb93bdb4f543a109180f2d49
Raghav Mittal <raghav.mittal@infor.com>
2019-04-24 16:55:09 -0700

    Post request working

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
-const express = require ('express')
-const app = express ()
+const request = require ('request')
 
-app.post ('http://roombooking.surrey.sfu.ca/edit_entry_handler.php', (req, res) => {
-    app.set ('Cookie', '_ga=GA1.2.316387562.1552265317; _gid=GA1.2.1030685302.1552265317; FAS_MRBS=rmittal')
-    app.set ('Referer', 'http://roombooking.surrey.sfu.ca/edit_entry.php?area=1&room=1&hour=7&minute=00&year=2019&month=4&day=25')
-    app.set ('Content-Type', 'application/x-www-form-urlencoded')
+let day = 30
+let month = 4
+let year = 2019
+let start_seconds = 32400
+let end_seconds = 32400 + 3600 + 3600
+let userID = 'vakansha'
+let currentHour = start_seconds/3600
 
-    res.send ({
+var formData = {
         name: 'Room booking',
         description: 'Study room for friends',
-        start_day: 25,
-        start_month:,
-        start_year:,
-        start_seconds:,
-        end_day:,
-        end_month:,
-        end_year:,
-        end_seconds:,
+        start_day: day,
+        start_month: month,
+        start_year: year,
+        start_seconds,
+        end_day: day,
+        end_month: month,
+        end_year: year,
+        end_seconds,
         area:1,
         rooms:[1],
         type:'E',
-        returl:'http://roombooking.surrey.sfu.ca/day.php?year={{year}}&month={{month}}&day={{day}}&area=1&room=1',
-        create_by:'rmittal',
+        returl: `http://roombooking.surrey.sfu.ca/day.php?year=${year}&month=${month}&day=${day}&area=1&room=1`,
+        create_by: userID,
         save_button:'Save'
-    })
+}
+
+request ({
+    headers: {
+        'Cookie': `_ga=GA1.2.316387562.1552265317; _gid=GA1.2.1030685302.1552265317; FAS_MRBS=${userID}`,
+        'Referer': `http://roombooking.surrey.sfu.ca/edit_entry.php?area=1&room=1&hour=${currentHour}&minute=00&year=${year}&month=${month}&day=${day}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    url: 'http://roombooking.surrey.sfu.ca/edit_entry_handler.php',
+    formData,
+    method: 'POST'
+}, (err, res, body) => {
+    if (res.statusCode === 302) console.log ('Booking confirmed')
+    else console.log ('Could not book the room')
 })
